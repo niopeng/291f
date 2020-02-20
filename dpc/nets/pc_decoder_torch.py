89ef030bfd226ca5329d3f245d02d79dca0cb025
Eddie <Mbfdcg@live.com>
2020-02-20 11:07:36 -0800

    Update pc_decoder_torch.py

--- dpc/nets/pc_decoder_torch.py
+++ dpc/nets/pc_decoder_torch.py
@@ -1,9 +1,12 @@
 # pytorching working on
 import numpy as np
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 
-class pcDecoder(torch.nn.Module):
+class pcDecoder(nn.Module):
+    '''input shape should be [batch_size, z_dim]. 
+       For rgb_deep_decoder, the size of outputs_all['conv_features'] shoud be [batch_size, 256*4*4].'''
     def __init__(self, cfg, afterConvSize=256*4*4):
         super(pcDecoder, self).__init__()
 
@@ -11,22 +14,22 @@ class pcDecoder(torch.nn.Module):
         self.fc_dim = cfg.fc_dim
         self.num_points = cfg.pc_num_points
         self.input_size = cfg.z_dim
-        self.tanh = torch.nn.Tanh()
-        self.sigmoid = torch.nn.Sigmoid()
-        self.act_func = torch.nn.LeakyReLU(negative_slope=0.2)
-        self.fc1 = torch.nn.Linear(self.input_size, self.num_points*3)
-        self.deep_fc1 = torch.nn.Linear(afterConvSize, self.fc_dim)
-        self.deep_fc2 = torch.nn.Linear(self.fc_dim, self.fc_dim)
-        self.deep_fc3 = torch.nn.Linear(self.fc_dim, self.fc_dim)
-        self.deep_fc_last = torch.nn.Linear(self.fc_dim, self.num_points*3)
-        self.shallow_fc_last = torch.nn.Linear(self.input_size, self.num_points*3)
+        self.tanh = nn.Tanh()
+        self.sigmoid = nn.Sigmoid()
+        self.act_func = nn.LeakyReLU(negative_slope=0.2)
+        self.fc1 = nn.Linear(self.input_size, self.num_points*3)
+        self.deep_fc1 = nn.Linear(afterConvSize, self.fc_dim)
+        self.deep_fc2 = nn.Linear(self.fc_dim, self.fc_dim)
+        self.deep_fc3 = nn.Linear(self.fc_dim, self.fc_dim)
+        self.deep_fc_last = nn.Linear(self.fc_dim, self.num_points*3)
+        self.shallow_fc_last = nn.Linear(self.input_size, self.num_points*3)
 
-        torch.nn.init.normal_(self.fc1.weight, std=cfg.pc_decoder_init_stddev)
-        torch.nn.init.normal_(self.deep_fc_last.weight, std=cfg.pc_decoder_init_stddev)
-        torch.nn.init.normal_(self.shallow_fc_last.weight, std=cfg.pc_decoder_init_stddev)
-        torch.nn.init.kaiming_normal_(self.deep_fc1.weight, a=0.2)
-        torch.nn.init.kaiming_normal_(self.deep_fc2.weight, a=0.2)
-        torch.nn.init.kaiming_normal_(self.deep_fc3.weight, a=0.2)
+        nn.init.normal_(self.fc1.weight, std=cfg.pc_decoder_init_stddev)
+        nn.init.normal_(self.deep_fc_last.weight, std=cfg.pc_decoder_init_stddev)
+        nn.init.normal_(self.shallow_fc_last.weight, std=cfg.pc_decoder_init_stddev)
+        nn.init.kaiming_normal_(self.deep_fc1.weight, a=0.2)
+        nn.init.kaiming_normal_(self.deep_fc2.weight, a=0.2)
+        nn.init.kaiming_normal_(self.deep_fc3.weight, a=0.2)
 
     def forward(self, inputs, outputs_all, is_training=True):
         batch_size = inputs.size()[0]
@@ -66,9 +69,9 @@ class pcDecoder(torch.nn.Module):
 #     num_points = cfg.pc_num_points
 #     init_stddev = cfg.pc_decoder_init_stddev
 #     batch_size = inputs.size()[0]
-#     fc1 = torch.nn.Linear(inputs.size()[1], num_points*3)
+#     fc1 = nn.Linear(inputs.size()[1], num_points*3)
 #     # nn.init.normal_ instead of tf.truncated_normal_initializer()
-#     torch.nn.init.normal_(fc1.weight, std=init_stddev)
+#     nn.init.normal_(fc1.weight, std=init_stddev)
 
 #     pts_raw = fc1(inputs)
 #     pred_pts = pts_raw.view(batch_size, num_points, 3)
@@ -80,20 +83,20 @@ class pcDecoder(torch.nn.Module):
 #     out = dict()
 #     out["xyz"] = pred_pts
 
-#     deep_fc1 = torch.nn.Linear(outputs_all["conv_features"].size()[1], cfg.fc_dim)
-#     deep_fc2 = torch.nn.Linear(cfg.fc_dim, cfg.fc_dim)
-#     deep_fc3 = torch.nn.Linear(cfg.fc_dim, cfg.fc_dim)
-#     deep_fc_last = torch.nn.Linear(cfg.fc_dim, num_points*3)
-#     shallow_fc_last = torch.nn.Linear(inputs.size()[1], num_points*3)
+#     deep_fc1 = nn.Linear(outputs_all["conv_features"].size()[1], cfg.fc_dim)
+#     deep_fc2 = nn.Linear(cfg.fc_dim, cfg.fc_dim)
+#     deep_fc3 = nn.Linear(cfg.fc_dim, cfg.fc_dim)
+#     deep_fc_last = nn.Linear(cfg.fc_dim, num_points*3)
+#     shallow_fc_last = nn.Linear(inputs.size()[1], num_points*3)
 
 #     # kaiming_normal_ instead of tf.contrib.layers.variance_scaling_initializer()
 #     # aka msra initialization
-#     torch.nn.init.kaiming_normal_(deep_fc1.weight, a=0.2)
-#     torch.nn.init.kaiming_normal_(deep_fc2.weight, a=0.2)
-#     torch.nn.init.kaiming_normal_(deep_fc3.weight, a=0.2)
+#     nn.init.kaiming_normal_(deep_fc1.weight, a=0.2)
+#     nn.init.kaiming_normal_(deep_fc2.weight, a=0.2)
+#     nn.init.kaiming_normal_(deep_fc3.weight, a=0.2)
 
-#     torch.nn.init.normal_(deep_fc_last.weight, std=init_stddev)
-#     torch.nn.init.normal_(shallow_fc_last.weight, std=init_stddev)
+#     nn.init.normal_(deep_fc_last.weight, std=init_stddev)
+#     nn.init.normal_(shallow_fc_last.weight, std=init_stddev)
 
 #     if cfg.pc_rgb:
 #         if cfg.pc_rgb_deep_decoder:
